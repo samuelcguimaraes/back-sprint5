69009ef7ea6a01ad86783e1f9046bcf4f5c642ea
samuel.guimaraes <samuel.guimaraes@cabal.com.br>
2021-07-06 09:45:56 -0300

    Refatora classe CardStatisticsResponseDto: - Retira herança de TreeMap e realiza uma composição adicionando uma variável do mesmo tipo

--- cards/src/main/java/br/com/rchlo/cards/dto/CardStatisticsResponseDto.java
+++ cards/src/main/java/br/com/rchlo/cards/dto/CardStatisticsResponseDto.java
@@ -5,29 +5,19 @@ import br.com.rchlo.cards.domain.Transaction;
 import java.time.YearMonth;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
-import java.util.Map;
 import java.util.TreeMap;
 
-public class CardStatisticsResponseDto extends TreeMap<YearMonth, CardStatisticsItemResponseDto> {
-
+public class CardStatisticsResponseDto {
+    
+    private final TreeMap<YearMonth, CardStatisticsItemResponseDto> cardStatistics = new TreeMap<>();
+    
     public void addItem(YearMonth yearMonth, List<Transaction> transactionsForYearMonth) {
-        DoubleSummaryStatistics transactionSummaryStatistics = transactionsForYearMonth.stream().mapToDouble(transaction -> transaction.getAmount().doubleValue()).summaryStatistics();
-        this.put(yearMonth, new CardStatisticsItemResponseDto(transactionSummaryStatistics));
+        DoubleSummaryStatistics transactionSummaryStatistics = transactionsForYearMonth.stream().mapToDouble(
+                transaction -> transaction.getAmount().doubleValue()).summaryStatistics();
+        this.cardStatistics.put(yearMonth, new CardStatisticsItemResponseDto(transactionSummaryStatistics));
     }
-
-    @Override
-    public CardStatisticsItemResponseDto remove(Object key) {
-        throw new UnsupportedOperationException();
+    
+    public TreeMap<YearMonth, CardStatisticsItemResponseDto> getCardStatistics() {
+        return this.cardStatistics;
     }
-
-    @Override
-    public void putAll(Map<? extends YearMonth, ? extends CardStatisticsItemResponseDto> m) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException();
-    }
-
 }
